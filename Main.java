64bb45812a4ab775f0827249355e75e8a304573b
finny <phineas.ehrlich2026@gbwl.org>
2025-01-17 14:40:16 +0000

    ERGTREWOGFKERWSDFOGWERGDFBO)EWergdfr

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 public class Main {
 
    public static void main(String []args) {
-      String[] cows = {"cow1", "cow2", "cow3"};
+      String[] cows = {"cow1", "cow2", "cow3", "cow4", "cow5"};
       System.out.println(cows[0]);
 
       int[] luckyNums = {37, 7, 77, 777};
@@ -19,15 +19,23 @@ public class Main {
          System.out.println(tftf[3]);
       }
 
-      double[] prices = new double[5];
-      String[] dogNames = new String[5];
+      double[] prices = {5000.0, 3000.0, 8000.0, 6000.0, 12000.0};
+      String[] dogNames = {"Elodie", "Elliot", "Elijah", "Mark", "Elena"};
 
       System.out.println(prices.length);
       System.out.println(dogNames.length);
 
-      prices = {5000.0, 3000.0, 8000.0, 6000.0, 12000.0};
-      dogNames = {"Elodie", "Elliot", "Elijah", "Mark", "Elena"};
-
       System.out.println(dogNames[0] + "the dog can be purchased for $" + prices[0]);
+
+      System.out.println("\n\n___________________________________________________________________");
+      
+      for (int i = 0; i < 4; i++) {
+         for (int o = 0; o < 4; o++) {
+            System.out.print(cows[i] + "|");
+            System.out.print(luckyNums[i] + "|");
+            System.out.print(mim[i] + "|");
+         }
+         System.out.println("\n___________________________________________________________________");
+      }
    }
 }
